55919febc64193a2af8942ed6465a4f5fb76dac9
kainar <kainar.ilyassov@gmail.com>
2024-03-05 02:09:24 +0500

    add shopping cart logic and product details page

--- src/lib/validators/query-validator.ts
+++ src/lib/validators/query-validator.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const QueryValidator = z.object({
   category: z.string().optional(),
-  sort: z.enum(["asc", "desc"]),
+  sort: z.enum(["asc", "desc"]).optional(),
   limit: z.number().optional(),
 });
 
