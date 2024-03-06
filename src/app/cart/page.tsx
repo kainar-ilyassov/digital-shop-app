a52d355c7c1b394ad0c24e9982ed5f4cfb918fe9
kainar <kainar.ilyassov@gmail.com>
2024-03-06 22:35:37 +0500

    refactor cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -26,9 +26,14 @@ const Page = () => {
   const productIds = items.map(({ product }) => product.id);
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   const cartTotal = items.reduce(
     (total, { product }) => total + product.price,
@@ -48,12 +53,12 @@ const Page = () => {
           <div
             className={cn("lg:col-span-7", {
               "rounded-lg border-2 border-dashed border-zinc-200 p-12":
-                isMounted && items.length === 0,
+                !items.length,
             })}
           >
             <h2 className="sr-only">Items in your shopping cart</h2>
 
-            {isMounted && items.length === 0 ? (
+            {!items.length ? (
               <div className="flex h-full flex-col items-center justify-center space-y-1">
                 <div
                   aria-hidden="true"
@@ -76,79 +81,78 @@ const Page = () => {
             <ul
               className={cn({
                 "divide-y divide-gray-200 border-b border-t border-gray-200":
-                  isMounted && items.length > 0,
+                  items.length,
               })}
             >
-              {isMounted &&
-                items.map(({ product }) => {
-                  const label = PRODUCT_CATEGORIES.find(
-                    (c) => c.value === product.category
-                  )?.label;
+              {items.map(({ product }) => {
+                const label = PRODUCT_CATEGORIES.find(
+                  (c) => c.value === product.category
+                )?.label;
 
-                  const { image } = product.images[0];
+                const { image } = product.images[0];
 
-                  return (
-                    <li key={product.id} className="flex py-6 sm:py-10">
-                      <div className="flex-shrink-0">
-                        <div className="relative h-24 w-24">
-                          {typeof image !== "string" && image.url ? (
-                            <Image
-                              fill
-                              src={image.url}
-                              alt="product image"
-                              className="h-full w-full rounded-md object-cover object-center sm:h-48 sm:w-48"
-                            />
-                          ) : null}
-                        </div>
+                return (
+                  <li key={product.id} className="flex py-6 sm:py-10">
+                    <div className="flex-shrink-0">
+                      <div className="relative h-24 w-24">
+                        {typeof image !== "string" && image.url ? (
+                          <Image
+                            fill
+                            src={image.url}
+                            alt="product image"
+                            className="h-full w-full rounded-md object-cover object-center sm:h-48 sm:w-48"
+                          />
+                        ) : null}
                       </div>
+                    </div>
 
-                      <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                          <div>
-                            <div className="flex justify-between">
-                              <h3 className="text-sm">
-                                <Link
-                                  href={`/product/${product.id}`}
-                                  className="font-medium text-gray-700 hover:text-gray-800"
-                                >
-                                  {product.name}
-                                </Link>
-                              </h3>
-                            </div>
+                    <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                      <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                        <div>
+                          <div className="flex justify-between">
+                            <h3 className="text-sm">
+                              <Link
+                                href={`/product/${product.id}`}
+                                className="font-medium text-gray-700 hover:text-gray-800"
+                              >
+                                {product.name}
+                              </Link>
+                            </h3>
+                          </div>
 
-                            <div className="mt-1 flex text-sm">
-                              <p className="text-muted-foreground">
-                                Category: {label}
-                              </p>
-                            </div>
-
-                            <p className="mt-1 text-sm font-medium text-gray-900">
-                              {formatPrice(product.price)}
+                          <div className="mt-1 flex text-sm">
+                            <p className="text-muted-foreground">
+                              Category: {label}
                             </p>
                           </div>
 
-                          <div className="mt-4 sm:mt-0 sm:pr-9 w-20">
-                            <div className="absolute right-0 top-0">
-                              <Button
-                                aria-label="remove product"
-                                onClick={() => removeItem(product.id)}
-                                variant="ghost"
-                              >
-                                <X className="h-5 w-5" aria-hidden="true" />
-                              </Button>
-                            </div>
-                          </div>
+                          <p className="mt-1 text-sm font-medium text-gray-900">
+                            {formatPrice(product.price)}
+                          </p>
                         </div>
 
-                        <p className="mt-4 flex space-x-2 text-sm text-gray-700">
-                          <Check className="h-5 w-5 flex-shrink-0 text-green-500" />
-
-                          <span>Eligible for instant delivery</span>
-                        </p>
+                        <div className="mt-4 sm:mt-0 sm:pr-9 w-20">
+                          <div className="absolute right-0 top-0">
+                            <Button
+                              aria-label="remove product"
+                              onClick={() => removeItem(product.id)}
+                              variant="ghost"
+                            >
+                              <X className="h-5 w-5" aria-hidden="true" />
+                            </Button>
+                          </div>
+                        </div>
                       </div>
-                    </li>
-                  );
-                })}
+
+                      <p className="mt-4 flex space-x-2 text-sm text-gray-700">
+                        <Check className="h-5 w-5 flex-shrink-0 text-green-500" />
+
+                        <span>Eligible for instant delivery</span>
+                      </p>
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           </div>
 
@@ -159,11 +163,7 @@ const Page = () => {
               <div className="flex items-center justify-between">
                 <p className="text-sm text-gray-600">Subtotal</p>
                 <p className="text-sm font-medium text-gray-900">
-                  {isMounted ? (
-                    formatPrice(cartTotal)
-                  ) : (
-                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
-                  )}
+                  {formatPrice(cartTotal)}
                 </p>
               </div>
 
@@ -172,11 +172,7 @@ const Page = () => {
                   <span>Flat Transaction Fee</span>
                 </div>
                 <div className="text-sm font-medium text-gray-900">
-                  {isMounted ? (
-                    formatPrice(fee)
-                  ) : (
-                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
-                  )}
+                  {formatPrice(fee)}
                 </div>
               </div>
 
@@ -185,11 +181,7 @@ const Page = () => {
                   Order Total
                 </div>
                 <div className="text-base font-medium text-gray-900">
-                  {isMounted ? (
-                    formatPrice(cartTotal + fee)
-                  ) : (
-                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
-                  )}
+                  {formatPrice(cartTotal + fee)}
                 </div>
               </div>
             </div>
