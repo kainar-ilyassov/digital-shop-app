1964e0d613e0dcc448a9ec92ad0d6dc8aa97053b
kainar <kainar.ilyassov@gmail.com>
2024-04-06 03:07:00 +0500

    security add

--- src/collections/products/products.ts
+++ src/collections/products/products.ts
@@ -1,22 +1,90 @@
+import {
+  AfterChangeHook,
+  BeforeChangeHook,
+} from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
 import { Product, User } from "../../payload-types";
-import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { stripe } from "../../lib/stripe";
 
 const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
-  const user = req.user as User;
+  const user = req.user;
 
   return { ...data, user: user.id };
 };
+
+const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
+  const fullUser = await req.payload.findByID({
+    collection: "users",
+    id: req.user.id,
+  });
+
+  if (fullUser && typeof fullUser === "object") {
+    const { products } = fullUser;
+
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === "object" ? product.id : product
+      ) || []),
+    ];
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    );
+
+    const dataToUpdate = [...createdProductIDs, doc.id];
+
+    await req.payload.update({
+      collection: "users",
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    });
+  }
+};
+
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    const userProductIDs = (user.products || []).reduce<Array<string>>(
+      (acc, product) => {
+        if (!product) return acc;
+        if (typeof product === "string") {
+          acc.push(product);
+        } else {
+          acc.push(product.id);
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    };
+  };
 
 export const Products: CollectionConfig = {
   slug: "products",
   admin: {
     useAsTitle: "name",
   },
-  access: {},
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
+    afterChange: [syncUser],
     beforeChange: [
       addUser,
       async (args) => {
@@ -30,11 +98,13 @@ export const Products: CollectionConfig = {
               unit_amount: Math.round(data.price * 100),
             },
           });
+
           const updated: Product = {
             ...data,
             stripeId: createdProduct.id,
             priceId: createdProduct.default_price as string,
           };
+
           return updated;
         } else if (args.operation === "update") {
           const data = args.data as Product;
@@ -49,6 +119,7 @@ export const Products: CollectionConfig = {
             stripeId: updatedProduct.id,
             priceId: updatedProduct.default_price as string,
           };
+
           return updated;
         }
       },
@@ -88,10 +159,7 @@ export const Products: CollectionConfig = {
       name: "category",
       label: "Category",
       type: "select",
-      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({
-        label,
-        value,
-      })),
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
       required: true,
     },
     {
@@ -108,9 +176,9 @@ export const Products: CollectionConfig = {
       type: "select",
       defaultValue: "pending",
       access: {
-        create: ({ req }) => req.user?.role === "admin",
-        read: ({ req }) => req.user?.role === "admin",
-        update: ({ req }) => req.user?.role === "admin",
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
       },
       options: [
         {
